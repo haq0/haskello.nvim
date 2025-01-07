9638de8c72041982b40273d630947f69c00c0e9b
haq <haq@haquire.dev>
2025-01-07 18:51:29 -0500

    a

--- lua/haskello/init.lua
+++ lua/haskello/init.lua
@@ -1,57 +1,53 @@
-local config = require("haskello.config")
-local formatters = require("haskello.formatters")
+local api = vim.api
+local formatters = require('haskello.formatters')
+local config = require('haskello.config')
 
 local M = {}
 
+local function format_async(formatter_name, args, bufnr)
+  bufnr = bufnr or api.nvim_get_current_buf()
+  local filename = api.nvim_buf_get_name(bufnr)
+
+  local cmd_args = formatters.get_format_command(formatter_name, args, filename)
+  if vim.tbl_isempty(cmd_args) then return end
+
+  local cmd = args.cmd or formatter_name
+
+  vim.fn.jobstart({cmd, unpack(cmd_args)}, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_exit = function(_, exit_code)
+      if exit_code == 0 then
+        local win = api.nvim_get_current_win()
+        local cursor = api.nvim_win_get_cursor(win)
+        vim.cmd('checktime')
+        api.nvim_win_set_cursor(win, cursor)
+      else
+        vim.notify("Formatting failed", vim.log.levels.ERROR)
+      end
+    end,
+  })
+end
+
 function M.format()
-  local cfg = config.options
-  local fmt = cfg.formatters[cfg.formatter]
-  if not fmt then
-    vim.notify(string.format("Invalid formatter: %s", cfg.formatter), vim.log.levels.ERROR)
+  local opts = config.options
+  local formatter = opts.formatters[opts.formatter]
+  if not formatter then
+    vim.notify("No formatter configured", vim.log.levels.ERROR)
     return
   end
 
-  local filename = vim.api.nvim_buf_get_name(0)
-  local args = formatters.build_args(cfg.formatter, fmt.args)
-  table.insert(args, filename)
-
-  local cmd = {fmt.cmd}
-  vim.list_extend(cmd, args)
-
-  local output = vim.fn.system(cmd)
-
-  if vim.v.shell_error == 0 then
-    vim.cmd("edit!")
-  else
-    vim.notify(string.format("Formatting failed:\n%s", output), vim.log.levels.ERROR)
-  end
-end
-
-function M.set_formatter(formatter_name)
-  if config.options.formatters[formatter_name] then
-    config.options.formatter = formatter_name
-    vim.notify(string.format("Switched to %s", formatter_name), vim.log.levels.INFO)
-  else
-    vim.notify(string.format("Invalid formatter: %s", formatter_name), vim.log.levels.ERROR)
-  end
+  format_async(opts.formatter, formatter.args)
 end
 
 function M.setup(opts)
   config.setup(opts)
 
-  vim.api.nvim_create_user_command("Haskello", M.format, {})
-  vim.api.nvim_create_user_command("HaskelloFormatter", function(cmd_opts)
-    M.set_formatter(cmd_opts.args)
-  end, {
-    nargs = 1,
-    complete = function()
-      return {"ormolu", "fourmolu", "hindent"}
-    end
-  })
-
   if config.options.format_on_save then
-    vim.api.nvim_create_autocmd("BufWritePre", {
+    local group = api.nvim_create_augroup("Haskello", { clear = true })
+    api.nvim_create_autocmd("BufWritePost", {
       pattern = "*.hs",
+      group = group,
       callback = M.format
     })
   end
--- test.hs
+++ test.hs
@@ -1,117 +1,134 @@
-
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
-import           Control.Monad
-import           Control.Exception
-import qualified Data.Map.Strict     as Map
-import qualified Data.Text.IO        as TIO
-import           Lib
-import           Stats.Processing
-import           Output.Formatter
-import           Options.Applicative
-import           System.Console.ANSI (hideCursor, showCursor)
-import           System.Directory    (doesDirectoryExist, doesFileExist,
-                                     removeDirectoryRecursive)
-import           System.Exit        (exitFailure)
-import           Text.Printf
-import           System.IO          (hPutStrLn, stderr)
-import           System.IO.Temp     (withSystemTempDirectory)
-import           System.Process     (callProcess, readCreateProcess, shell)
-import           Data.Maybe         (mapMaybe)
+import Control.Exception
+import Control.Monad
+import qualified Data.Map.Strict as Map
+import Data.Maybe (mapMaybe)
+import qualified Data.Text.IO as TIO
+import Lib
+import Options.Applicative
+import Output.Formatter
+import Stats.Processing
+import System.Console.ANSI (hideCursor, showCursor)
+import System.Directory
+  ( doesDirectoryExist
+  , doesFileExist
+  , removeDirectoryRecursive
+  )
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (callProcess, readCreateProcess, shell)
+import Text.Printf
 
 data Options = Options
-  { optVerbose      :: Bool
-  , optGit          :: Bool
-  , optPath         :: FilePath
-  , optIgnoreDirs   :: [FilePath]
-  , optIgnoreLangs  :: [String]
+  { optVerbose :: Bool
+  , optGit :: Bool
+  , optPath :: FilePath
+  , optIgnoreDirs :: [FilePath]
+  , optIgnoreLangs :: [String]
   , optIgnoreHidden :: Bool
   , optDetectLicense :: Bool
   , optOutputFormat :: OutputFormat
-  } deriving (Show)
+  }
+  deriving (Show)
 
 stringToLanguage :: String -> Maybe Language
 stringToLanguage = \case
-    "ada" -> Just Ada
-    "asm" -> Just Assembly
-    "awk" -> Just AWK
-    "bash" -> Just Bash
-    "c" -> Just C
-    "cpp" -> Just CPP
-    "cs" -> Just CSharp
-    "css" -> Just CSS
-    "go" -> Just Go
-    "hs" -> Just Haskell
-    "html" -> Just HTML
-    "java" -> Just Java
-    "js" -> Just JavaScript
-    "json" -> Just JSON
-    "py" -> Just Python
-    "rb" -> Just Ruby
-    "rs" -> Just Rust
-    "scala" -> Just Scala
-    "sql" -> Just SQL
-    "swift" -> Just Swift
-    "ts" -> Just TypeScript
-    "xml" -> Just XML
-    other -> Just (Other other)
+  "ada" -> Just Ada
+  "asm" -> Just Assembly
+  "awk" -> Just AWK
+  "bash" -> Just Bash
+  "c" -> Just C
+  "cpp" -> Just CPP
+  "cs" -> Just CSharp
+  "css" -> Just CSS
+  "go" -> Just Go
+  "hs" -> Just Haskell
+  "html" -> Just HTML
+  "java" -> Just Java
+  "js" -> Just JavaScript
+  "json" -> Just JSON
+  "py" -> Just Python
+  "rb" -> Just Ruby
+  "rs" -> Just Rust
+  "scala" -> Just Scala
+  "sql" -> Just SQL
+  "swift" -> Just Swift
+  "ts" -> Just TypeScript
+  "xml" -> Just XML
+  other -> Just (Other other)
 
 parseOptions :: Parser Options
 parseOptions =
   Options
-    <$> switch 
-        (long "verbose" 
-         <> short 'v' 
-         <> help "Enable verbose output")
-    <*> switch 
-        (long "git" 
-         <> help "Clone a Git repository from the provided URL")
-    <*> argument str
-        (metavar "PATH" 
-         <> help "Path to analyze or URL of the repository if --git is specified")
-    <*> many 
-        (strOption
-          (long "ignore-dir"
-           <> metavar "DIR"
-           <> help "Directory to ignore (can be specified multiple times)"))
-    <*> many 
-        (strOption
-          (long "ignore-lang"
-           <> metavar "LANG"
-           <> help "Language to ignore (can be specified multiple times, e.g. js,py,rb)"))
+    <$> switch
+      ( long "verbose"
+          <> short 'v'
+          <> help "Enable verbose output"
+      )
     <*> switch
-        (long "ignore-hidden"
-         <> help "Ignore hidden directories (starting with .)")
+      ( long "git"
+          <> help "Clone a Git repository from the provided URL"
+      )
+    <*> argument
+      str
+      ( metavar "PATH"
+          <> help "Path to analyze or URL of the repository if --git is specified"
+      )
+    <*> many
+      ( strOption
+          ( long "ignore-dir"
+              <> metavar "DIR"
+              <> help "Directory to ignore (can be specified multiple times)"
+          )
+      )
+    <*> many
+      ( strOption
+          ( long "ignore-lang"
+              <> metavar "LANG"
+              <> help "Language to ignore (can be specified multiple times, e.g. js,py,rb)"
+          )
+      )
     <*> switch
-        (long "detect-license"
+      ( long "ignore-hidden"
+          <> help "Ignore hidden directories (starting with .)"
+      )
+    <*> switch
+      ( long "detect-license"
           <> short 'l'
-          <> help "Detect software licenses in the codebase")
-    <*> option auto
-        (long "output"
-         <> short 'o'
-         <> metavar "FORMAT"
-         <> value Console
-         <> help "Output format: json, csv, markdown")
+          <> help "Detect software licenses in the codebase"
+      )
+    <*> option
+      auto
+      ( long "output"
+          <> short 'o'
+          <> metavar "FORMAT"
+          <> value Console
+          <> help "Output format: json, csv, markdown"
+      )
 
 opts :: ParserInfo Options
 opts =
   info
     (parseOptions <**> helper)
-    (fullDesc
-     <> progDesc "Count lines of code in source files or a Git repository"
-     <> header "hydra - a source code line counter")
+    ( fullDesc
+        <> progDesc "Count lines of code in source files or a Git repository"
+        <> header "hydra - a source code line counter"
+    )
 
 makeConfig :: Options -> FilePath -> Config
-makeConfig Options{..} path = Config 
-  { configPath = path
-  , configVerbose = optVerbose
-  , configIgnoreDirs = defaultIgnoreDirs ++ optIgnoreDirs ++ hiddenDirs
-  , configIgnoreLangs = mapMaybe stringToLanguage optIgnoreLangs
-  }
+makeConfig Options {..} path =
+  Config
+    { configPath = path
+    , configVerbose = optVerbose
+    , configIgnoreDirs = defaultIgnoreDirs ++ optIgnoreDirs ++ hiddenDirs
+    , configIgnoreLangs = mapMaybe stringToLanguage optIgnoreLangs
+    }
   where
     defaultIgnoreDirs = ["dist", "dist-newstyle", ".stack-work", "node_modules"]
     hiddenDirs = if optIgnoreHidden then [".git", ".hie", ".vscode"] else []
@@ -124,20 +141,23 @@ main = do
   flip finally showCursor $ do
     if optGit options
       then withSystemTempDirectory "hydra-clone" $ \tempDir -> do
-        when (optVerbose options) $ 
-          putStrLn $ "Cloning repository: " ++ optPath options
+        when (optVerbose options) $
+          putStrLn $
+            "Cloning repository: " ++ optPath options
 
         let gitCommand = "git clone " ++ optPath options ++ " " ++ tempDir
         if optVerbose options
           then callProcess "git" ["clone", optPath options, tempDir]
           else do
-            _ <- readCreateProcess
-                   (shell $ gitCommand ++ " > /dev/null 2>&1")
-                   ""
+            _ <-
+              readCreateProcess
+                (shell $ gitCommand ++ " > /dev/null 2>&1")
+                ""
             return ()
 
-        when (optVerbose options) $ 
-          putStrLn $ "Repository cloned to: " ++ tempDir
+        when (optVerbose options) $
+          putStrLn $
+            "Repository cloned to: " ++ tempDir
 
         when (optDetectLicense options) $ do
           when (optVerbose options) $
@@ -146,11 +166,13 @@ main = do
           unless (null licenses) $ do
             putStrLn "\nDetected Licenses:"
             putStrLn "-------------------"
-            forM_ licenses $ \LicenseInfo{..} -> do
-              putStrLn $ printf "%-12s (%.0f%% confidence) in %s"
-                (show liLicense)
-                (liConfidence * 100)
-                liFile
+            forM_ licenses $ \LicenseInfo {..} -> do
+              putStrLn $
+                printf
+                  "%-12s (%.0f%% confidence) in %s"
+                  (show liLicense)
+                  (liConfidence * 100)
+                  liFile
             putStrLn ""
 
         results <- countLines (makeConfig options tempDir)
@@ -165,12 +187,13 @@ main = do
             fmt -> TIO.putStrLn $ formatOutput results fmt
 
         when (optVerbose options) $
-          putStrLn $ "Cleaning up temporary directory: " ++ tempDir
+          putStrLn $
+            "Cleaning up temporary directory: " ++ tempDir
         removeDirectoryRecursive tempDir
-
       else do
-        when (optVerbose options) $ 
-          putStrLn $ "Analyzing path: " ++ optPath options
+        when (optVerbose options) $
+          putStrLn $
+            "Analyzing path: " ++ optPath options
 
         isFile <- doesFileExist (optPath options)
         isDir <- doesDirectoryExist (optPath options)
@@ -186,11 +209,13 @@ main = do
           unless (null licenses) $ do
             putStrLn "\nDetected Licenses:"
             putStrLn "-------------------"
-            forM_ licenses $ \LicenseInfo{..} -> do
-              putStrLn $ printf "%-12s (%.0f%% confidence) in %s"
-                (show liLicense)
-                (liConfidence * 100)
-                liFile
+            forM_ licenses $ \LicenseInfo {..} -> do
+              putStrLn $
+                printf
+                  "%-12s (%.0f%% confidence) in %s"
+                  (show liLicense)
+                  (liConfidence * 100)
+                  liFile
             putStrLn ""
 
         results <- countLines (makeConfig options (optPath options))
@@ -203,4 +228,3 @@ main = do
           else case optOutputFormat options of
             Console -> prettyPrint results
             fmt -> TIO.putStrLn $ formatOutput results fmt
-
